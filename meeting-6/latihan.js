1a6894bd73653e24caddffb15e45583f4f92958d
zhafran0408 <zhafran1108@student.abudzar.sch.id>
2025-02-21 09:10:44 +0700

    meeting-6

---
+++ meeting-6/latihan.js
@@ -0,0 +1,16 @@
+// nomer 1
+let dressSize = 45;
+console.log(typeof dressSize);
+
+dressSize = String(dressSize);
+console.log(typeof dressSize);
+
+dressSize = +45;
+console.log(typeof dressSize);
+
+// nomer2
+let isLight = `true`;
+console.log(isLight);
+
+isLight = String(isLight);
+console.log(typeof isLight);

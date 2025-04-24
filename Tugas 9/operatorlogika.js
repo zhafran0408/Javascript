af8c62355e45f82212253b375ffa46efa92c3240
zhafran0408 <zhafran1108@student.abudzar.sch.id>
2025-04-24 11:00:43 +0700

    Tugas 9

---
+++ Tugas 9/operatorlogika.js
@@ -0,0 +1,25 @@
+let nilai_ipa = 81;
+let nilai_ips = 91;
+let nilai_mtk = 91;
+if (nilai_ipa >= 91 && nilai_mtk >= 91 && nilai_ips >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+
+let hasil_ipa = 81;
+let hasil_ips = 91;
+let hasil_mtk = 91;
+if (hasil_ipa >= 91 || hasil_ips >= 91 || hasil_mtk >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("tidak lulus");
+}
+
+let silahkan_login = true;
+if (silahkan_login) {
+  console.log("anda belum login");
+}
+else{
+  console.log("anda sudah login");
+}

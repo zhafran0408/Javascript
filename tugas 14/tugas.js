0b91e24bf01167ca8742cee8de4572e3ed5ae590
zhafran0408 <zhafran1108@student.abudzar.sch.id>
2025-05-08 11:26:50 +0700

    14

---
+++ tugas 14/tugas.js
@@ -0,0 +1,17 @@
+// soal 1
+function calculateArea(lenght, width) {
+  return lenght * width;
+}
+console.log(calculateArea(5,2));
+// jawaban
+// Expretion function
+const calculateraea = function (length, width) {
+  return length * width;
+};
+calculateraea(5, 5);
+console.log(calculateraea(5,5));
+
+//  Arrow Function
+const calculaterea = (lenght, width) => lenght * width;
+calculaterea(5, 2);
+console.log(calculaterea(5,10));
